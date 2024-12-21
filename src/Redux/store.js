dc4209f5edc3a22eb2d337ae6cea5c2533e8dd74
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-21 16:01:47 +0530

    Basic authSlice code added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "./Slices/authSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSliceReducer,
+  },
   devTools: true,
 });
 
