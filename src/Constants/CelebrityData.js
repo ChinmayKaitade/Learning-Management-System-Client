670dc37a753b6549d623b8167b5bccb0f8bd0516
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-25 08:14:52 +0530

    CarouselSlide component and CelebrityData file added and About us page cleanup done

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,43 @@
+import nelsonMandela from "../Assets/Images/QuotesPersonalityImage/nelsonMandela.png";
+import apj from "../assets/Images/QuotesPersonalityImage/apj.png";
+import einstein from "../assets/Images/QuotesPersonalityImage/einstein.png";
+import steveJobs from "../assets/Images/QuotesPersonalityImage/steveJobs.png";
+import billGates from "../assets/Images/QuotesPersonalityImage/billGates.png";
+
+
+export const celebrities = [
+  {
+    title: "Nelson Mandela",
+    description:
+      "Education is the most powerful tool you can use to change the world.",
+    image: nelsonMandela,
+    slideNumber: 1,
+  },
+  {
+    title: "APJ Abdul Kalam",
+    description:
+      "Learning gives creativity, creativity leads to thinking,thinking provides knowledge, knowledge makes you great.",
+    image: apj,
+    slideNumber: 2,
+  },
+  {
+    title: "Albert Einstein",
+    description:
+      "Education is not the learning of facts, but the training of the mind to think.",
+    image: einstein,
+    slideNumber: 3,
+  },
+  {
+    title: "Steve Jobs",
+    description: "Innovation distinguishes between a leader and a follower.",
+    image: steveJobs,
+    slideNumber: 4,
+  },
+  {
+    title: "Bill Gates",
+    description:
+      "Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important.",
+    image: billGates,
+    slideNumber: 5,
+  },
+];
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import HomeLayout from "../Layouts/HomeLayout";
 import aboutMainImage from "../assets/Images/aboutMainImage.png";
 
-import nelsonMandela from "../Assets/Images/QuotesPersonalityImage/nelsonMandela.png";
-import apj from "../assets/Images/QuotesPersonalityImage/apj.png";
-import einstein from "../assets/Images/QuotesPersonalityImage/einstein.png";
-import steveJobs from "../assets/Images/QuotesPersonalityImage/steveJobs.png";
-import billGates from "../assets/Images/QuotesPersonalityImage/billGates.png";
+import CarouselSlide from "../Components/CarouselSlide";
+import { celebrities } from "../Constants/CelebrityData";
 
 const AboutUs = () => {
   return (
@@ -44,135 +41,14 @@ const AboutUs = () => {
 
         {/* top personalities quotes section */}
         <div className="carousel m-auto w-1/2 my-16">
-          <div id="slide1" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              {/* for personality image */}
-              <img
-                className="w-40 rounded-full border-2 border-gray-400"
-                src={nelsonMandela}
-                alt="Nelson Mandela"
+          {celebrities &&
+            celebrities.map((celebrity) => (
+              <CarouselSlide
+                {...celebrity}
+                key={celebrity.slideNumber}
+                totalSlides={celebrities.length}
               />
-              {/* for writing the quotes */}
-              <p className="text-xl text-gray-200">
-                "Education is the most powerful tool you can use to change the
-                world."
-              </p>
-              {/* for personality name */}
-              <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide5" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide2" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-
-          <div id="slide2" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              {/* for personality image */}
-              <img
-                className="w-40 rounded-full border-2 border-gray-400"
-                src={apj}
-                alt="APJ Abdul Kalam"
-              />
-              {/* for writing the quotes */}
-              <p className="text-xl text-gray-200">
-                "Learning gives creativity, creativity leads to thinking,
-                thinking provides knowledge, knowledge makes you great."
-              </p>
-              {/* for personality name */}
-              <h3 className="text-2xl font-semibold">A. P. J. Abdul Kalam</h3>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-
-          <div id="slide3" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              {/* for personality image */}
-              <img
-                className="w-40 rounded-full border-2 border-gray-400"
-                src={einstein}
-                alt="einstein"
-              />
-              {/* for writing the quotes */}
-              <p className="text-xl text-gray-200">
-                "Education is not the learning of facts, but the training of the
-                mind to think."
-              </p>
-              {/* for personality name */}
-              <h3 className="text-2xl font-semibold">Albert Einstein</h3>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide4" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-
-          <div id="slide4" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              {/* for personality image */}
-              <img
-                className="w-40 rounded-full border-2 border-gray-400"
-                src={steveJobs}
-                alt="Steve Jobs"
-              />
-              {/* for writing the quotes */}
-              <p className="text-xl text-gray-200">
-                "Innovation distinguishes between a leader and a follower."
-              </p>
-              {/* for personality name */}
-              <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide5" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-
-          <div id="slide5" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              {/* for personality image */}
-              <img
-                className="w-40 rounded-full border-2 border-gray-400"
-                src={billGates}
-                alt="Bill Gates"
-              />
-              {/* for writing the quotes */}
-              <p className="text-xl text-gray-200">
-                "Technology is just a tool. In terms of getting the kids working
-                together and motivating them, the teacher is the most
-                important."
-              </p>
-              {/* for personality name */}
-              <h3 className="text-2xl font-semibold">Bill Gates</h3>
-            </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide1" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
+            ))}
         </div>
       </div>
     </HomeLayout>
