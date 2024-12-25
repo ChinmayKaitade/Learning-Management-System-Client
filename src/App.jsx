68a99f10c4da03f22b6f25a178d8a1c74edd1115
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-25 07:40:31 +0530

    About us page code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import AboutUs from "./Pages/AboutUs";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </>
   );
