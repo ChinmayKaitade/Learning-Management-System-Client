88a6db01316faa70d8059f96d7f82445a8a72560
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-25 08:37:27 +0530

    404 NotFound page code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
+import NotFound from "./Pages/NotFound";
 
 const App = () => {
   return (
@@ -8,6 +9,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
+
+
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </>
   );
