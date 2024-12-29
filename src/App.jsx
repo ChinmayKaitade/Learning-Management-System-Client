d2d37883d786880a479d9e08a98d89d90b06053f
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-29 16:42:43 +0530

    Contact Page code added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NotFound from "./Pages/NotFound";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import CourseList from "./Pages/Course/CourseList";
+import Contact from "./Pages/Contact";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/courses" element={<CourseList />} />
+        <Route path="/contact" element={<Contact />} />
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
