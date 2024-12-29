583d9464ec3659fa201699f442e933506ed72a5f
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-12-29 16:50:10 +0530

    Denied Page code added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import CourseList from "./Pages/Course/CourseList";
 import Contact from "./Pages/Contact";
+import Denied from "./Pages/Denied";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/about" element={<AboutUs />} />
         <Route path="/courses" element={<CourseList />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
