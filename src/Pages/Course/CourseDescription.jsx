f56af7593b7bb7f83569e65c8ae0f9d3a690b814
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-02 16:02:03 +0530

    Checkout Payment Page code added

--- src/Pages/Course/CourseDescription.jsx
+++ src/Pages/Course/CourseDescription.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import HomeLayout from "../../Layouts/HomeLayout";
 
 const CourseDescription = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
+  
   const { role, data } = useSelector((state) => state.auth);
 
   return (
@@ -44,8 +46,11 @@ const CourseDescription = () => {
                   Watch Lectures
                 </button>
               ) : (
-                <button className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
-                  Subscribe to Course
+                <button
+                  onClick={() => navigate("/checkout")}
+                  className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300"
+                >
+                  Subscribe
                 </button>
               )}
             </div>
--- src/Redux/Slices/RazorpaySlice.js
+++ src/Redux/Slices/RazorpaySlice.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { toast } from "react-hot-toast";
-import axiosInstance from "../Helper/axiosInstance";
-
+import toast from "react-hot-toast";
+import axiosInstance from "../../Helpers/axiosInstance";
 const initialState = {
   key: "",
   subscription_id: "",
@@ -11,8 +10,7 @@ const initialState = {
   monthlySalesRecord: [],
 };
 
-// function to get the api key
-export const getRazorPayId = createAsyncThunk("/razorPayId/get", async () => {
+export const getRazorPayId = createAsyncThunk("/razorpay/getId", async () => {
   try {
     const response = await axiosInstance.get("/payments/razorpay-key");
     return response.data;
@@ -21,7 +19,6 @@ export const getRazorPayId = createAsyncThunk("/razorPayId/get", async () => {
   }
 });
 
-// function to purchase the course bundle
 export const purchaseCourseBundle = createAsyncThunk(
   "/purchaseCourse",
   async () => {
@@ -34,64 +31,57 @@ export const purchaseCourseBundle = createAsyncThunk(
   }
 );
 
-// function to verify the user payment
 export const verifyUserPayment = createAsyncThunk(
   "/payments/verify",
-  async (paymentDetail) => {
+  async (data) => {
     try {
       const response = await axiosInstance.post("/payments/verify", {
-        razorpay_payment_id: paymentDetail.razorpay_payment_id,
-        razorpay_subscription_id: paymentDetail.razorpay_subscription_id,
-        razorpay_signature: paymentDetail.razorpay_signature,
+        razorpay_payment_id: data.razorpay_payment_id,
+        razorpay_subscription_id: data.razorpay_subscription_id,
+        razorpay_signature: data.razorpay_signature,
       });
-      return response?.data;
-    } catch (error) {
-      toast.error("error?.response?.data?.message");
-    }
-  }
-);
-
-// function to get all the payment record
-export const getPaymentRecord = createAsyncThunk(
-  "/payments/record",
-  async () => {
-    try {
-      const res = axiosInstance.get("/payments?count=100");
-      toast.promise(res, {
-        loading: "Getting the payments record",
-        success: (data) => {
-          return data?.data?.message;
-        },
-        error: "Failed to get payment records",
-      });
-
-      const response = await res;
-      return response.data;
-    } catch (error) {
-      toast.error("Operation failed");
-    }
-  }
-);
-
-// function to cancel the course bundle subscription
-export const cancelCourseBundle = createAsyncThunk(
-  "/payments/unsubscribe",
-  async () => {
-    try {
-      const res = axiosInstance.post("/payments/unsubscribe");
-      toast.promise(res, {
-        loading: "Unsubscribing the bundle",
-        success: "Bundle UnSubscribe Successfully",
-        error: "Failed to UnSubscribe the bundle",
-      });
-      const response = await res;
       return response.data;
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
   }
 );
-
+export const getPaymentRecord = createAsyncThunk(
+  "/payments/record",
+  async () => {
+    try {
+      const response = axiosInstance.get("/payments?count=100");
+      toast.promise(response, {
+        loading: "Getting the payment records",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to get payment records",
+      });
+      return (await response).data;
+    } catch (error) {
+      toast.error("Operation failed");
+    }
+  }
+);
+export const cancelCourseBundle = createAsyncThunk(
+  "/payments/cancel",
+  async () => {
+    try {
+      const response = axiosInstance.post("/payments/unsubscribe");
+      toast.promise(response, {
+        loading: "unsubscribing the bundle",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to ubsubscrive",
+      });
+      return (await response).data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
 const razorpaySlice = createSlice({
   name: "razorpay",
   initialState,
@@ -105,6 +95,12 @@ const razorpaySlice = createSlice({
         state.subscription_id = action?.payload?.subscription_id;
       })
       .addCase(verifyUserPayment.fulfilled, (state, action) => {
+        console.log(action);
+        toast.success(action?.payload?.message);
+        state.isPaymentVerified = action?.payload?.success;
+      })
+      .addCase(verifyUserPayment.rejected, (state, action) => {
+        console.log(action);
         toast.success(action?.payload?.message);
         state.isPaymentVerified = action?.payload?.success;
       })
@@ -115,5 +111,4 @@ const razorpaySlice = createSlice({
       });
   },
 });
-
 export default razorpaySlice.reducer;
